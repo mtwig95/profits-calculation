76e49c2103f5175f7e3940fac4e633235efbbd81
Maytal Twig <61428701+mtwig95@users.noreply.github.com>
2021-12-02 14:12:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 
 <?PHP
+$username = 'tzinch';
+$password = "r#eD21mA%gNU";
+$remote_url = 'https://www.become.co/api/rest/test/';
+
+// Create a stream
+$opts = array(
+    'http'=>array(
+        'method'=>"GET",
+        'header' => "Authorization: Basic " . base64_encode("$username:$password")
+    )
+);
+
+$context = stream_context_create($opts);
+
+// Open the file using the HTTP headers set above
+$file = file_get_contents($remote_url, false, $context);
+
+$fp = fopen('data.json', 'w');
+fwrite($fp, json_encode($file));
+fclose($fp);
+
 // Getting data from the file
 $json_a = json_decode(file_get_contents("data.json"), true);
 $data = json_decode($json_a)->{'data'};
